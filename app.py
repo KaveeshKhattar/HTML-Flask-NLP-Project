3313b1fb36613445380a44c4b1ac8224359df345
Kaveesh Khattar <kaveeshkhattar@gmail.com>
2023-09-05 22:48:37 +0530

    Added SQLite Integration

--- app.py
+++ app.py
@@ -1,40 +1,50 @@
 from flask import Flask, render_template, request, redirect
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle5 as pickle
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3 as sql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sentmient.db'  # Replace with your database file path
-db = SQLAlchemy(app)
 
-app.app_context().push()
+conn = sql.connect('database.db')
+# print("Opened database successfully")
 
-class Query_Sentiment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    query = db.Column(db.String(200), unique=True, nullable=False)
-    sentiment = db.Column(db.String(40), unique=True, nullable=False)
-
-    def __init__(self, query, sentiment):
-        self.query = query
-        self.sentiment = sentiment
+# conn.execute('CREATE TABLE sentiment (id INTEGER NOT NULL PRIMARY KEY, query TEXT, sentiment TEXT)')
+# print("Table created successfully")
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template('index.html')
     elif request.method == "POST":
+
+        query = "Your query"
+        sentiment = "Positive"
+
+
         query = request.form.get("name", "I am HAPPY")
 
         loaded_vectorizer = pickle.load(open('vectorizer.pickle', 'rb'))        
         loaded_model = pickle.load(open('classification.model', 'rb'))
-        sentiment = loaded_model.predict(loaded_vectorizer.transform([query]))[0]
+        sentiment = loaded_model.predict(loaded_vectorizer.transform([query]))[0]        
 
-        new_query_sentiment = Query_Sentiment(query=query, sentiment=sentiment)
+        try:
+            with sql.connect("database.db") as con:
+                print("OK1")
+                cur = con.cursor()
+                print("OK2")
+                cur.execute("INSERT INTO sentiment (query, sentiment) VALUES (?,?)", (query, sentiment))
+                print("OK3")
+                con.commit()
+                con.close()
+                print("PASS")
 
+        except:
+            con.rollback()
+            print("FAIL")
         
-        db.session.add(new_query_sentiment)
-        db.session.commit()
-        return redirect('/history')
+        finally:
+            print("HERE WE COME")
+            return redirect('/history')
     
 
         # return render_template('emotion.html', name=sentiment)
@@ -43,8 +53,53 @@ def index():
 
 @app.route('/history')
 def history():
-    histories = Query_Sentiment.query.order_by(Query_Sentiment.id)
-    return render_template("history.html", history=histories)
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+    
+    cur = con.cursor()
+    history = cur.execute("select * from sentiment")    
+
+    print(history)
+    
+    return render_template("history.html", history=history)
+
+
+@app.route('/delete', methods=["POST"])
+def delete():
+    con = sql.connect("database.db")
+    cur = con.cursor()
+
+    id = request.form.get("id")
+    print("ID:", id)
+    if id:
+        cur.execute("DELETE FROM sentiment WHERE id = ?", [id])
+        con.commit()
+        con.close()
+
+    return redirect('/history')
+
+@app.route('/update', methods=["POST"])
+def update():
+
+    con = sql.connect("database.db")
+    cur = con.cursor()
+
+    query = request.form.get("query", "I am HAPPY")
+    id = request.form.get("id", "1")
+    print("Updating ID:", id)
+
+    if query and id:
+        
+        loaded_vectorizer = pickle.load(open('vectorizer.pickle', 'rb'))        
+        loaded_model = pickle.load(open('classification.model', 'rb'))
+        sentiment = loaded_model.predict(loaded_vectorizer.transform([query]))[0]        
+
+        cur.execute("UPDATE sentiment SET query = ?, sentiment = ? WHERE id = ?", (query, sentiment, id))
+        con.commit()
+        con.close()
+
+    return redirect('/history')
+
 
 if __name__ == '__main__':    
     app.run(debug=True)
